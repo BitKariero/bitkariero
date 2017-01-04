a56c3888dc6bd5e994e412efc053651bea27d098
George Pîrlea <george@dranov.ro>
2017-01-04 23:13:11 +0000

    Add identity page, add record types, fix btn CSS

--- app/common.jsx
+++ app/common.jsx
@@ -2,9 +2,23 @@ export const tabs = [
   {key: 'identity', label: 'Identity', icon: 'user'},
   {key: 'records', label: 'Records', icon: 'file text outline'},
   {key: 'approval-queue', label: 'Approval queue', icon: 'hourglass outline'},
-  {key: 'resume', label: 'Résumé', icon: 'signup'}
+  {key: 'resume', label: 'Résumé', icon: 'signup'},
 ];
 
 export const recordTypes = [
-  {key: 'reference', value: 'reference', text: 'Reference' }
+  {key: 'reference', value: 'reference', text: 'Reference'},
+  {key: 'certificate', value: 'certificate', text: 'Certificate'},
+  {key: 'diploma', value: 'diploma', text: 'Diploma'},
+];
+
+export const identityFields = {
+  name: {key: 'name', label: 'Name', icon: '', shownInHeader: true},
+  shortdesc: {key: 'shortdesc', label: '', icon: '', shownInHeader: true},
+  birthdate: {key: 'birthdate', label: 'Birth date', icon: 'calendar'},
+  email: {key: 'email', label:' E-mail', icon: 'mail outline'},
+  longdesc: {key: 'longdesc', label: 'Description', icon: 'info circle'},
+};
+
+export const identityDisplayOrder = [
+  'name', 'shortdesc', 'birthdate', 'email', 'longdesc'
 ];
--- app/components/identity.jsx
+++ app/components/identity.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
-import { Grid, Card, Icon, Input } from 'semantic-ui-react'
+import { Grid, Card, Header, Image, Segment, List, Icon, Label, Divider } from 'semantic-ui-react'
+import { identityFields, identityDisplayOrder } from '../common.jsx';
+
 
 export default class BK_Identity extends React.Component {
   constructor(props) {
@@ -14,13 +16,13 @@ export default class BK_Identity extends React.Component {
     const { identity } = this.props;
 
     return (
-      <Grid stackable columns={2}>
-        <Grid.Column width={4}>
-          <BK_IdentityCard identity={identity} />
-        </Grid.Column>
-
+      <Grid container stackable reversed='mobile' columns={2}>
         <Grid.Column width={12}>
           <BK_IdentityEditable identity={identity} />
+        </Grid.Column>
+
+        <Grid.Column width={4}>
+          <BK_IdentityCard identity={identity} />
         </Grid.Column>
       </Grid>
     )
@@ -32,12 +34,15 @@ class BK_IdentityCard extends React.Component {
     const { identity } = this.props;
 
     return (
-      <Card
-        image='img/person.jpg'
-        header={identity.name}
-        meta={'Born ' + identity.birthdate}
-        description={identity.desc}
-      />
+      <div>
+        <Card
+          image='img/person.jpg'
+          header={identity.name}
+          meta={'Born ' + identity.birthdate}
+          description={identity.shortdesc}
+          id='bk-identity-card'
+        />
+      </div>
     )
   }
 }
@@ -48,9 +53,59 @@ class BK_IdentityEditable extends React.Component {
 
     return (
       <div>
-        <h1>{identity.name}</h1>
-        <h2>Born {identity.birthdate}</h2>
-        <h2>{identity.desc}</h2>
+        <Header as='h2' attached='top'>
+          <Grid columns={2}>
+            <Grid.Column width={2} verticalAlign='middle'>
+              <Image shape='circular' src='img/person.jpg' id="bk-identity-small-picture"/>
+            </Grid.Column>
+
+            <Grid.Column width={14} verticalAlign='middle'>
+              <Grid.Row>
+                <Grid.Column>
+                  <span>{identity.name}</span>
+                </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row>
+                <Grid.Column>
+                  <span id="bk-identity-shortdesc">{identity.shortdesc}</span>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid.Column>
+          </Grid>
+        </Header>
+
+        <Segment attached>
+          <List>
+          { identityDisplayOrder.map(function(idField, i) {
+            return (
+              <div>
+              { identity[idField] && !identityFields[idField].shownInHeader &&
+                <div>
+                  <List.Item>
+                    <List.Content>
+                      <Label>
+                        <Icon name={identityFields[idField].icon} />
+                        {identityFields[idField].label}
+                      </Label>
+
+                      { idField == 'longdesc' ?
+                        <p className='bk-identity-field'>{identity[idField]}</p>
+                                              :
+                        <span className='bk-identity-field'>{identity[idField]}</span>
+                      }
+                    </List.Content>
+                  </List.Item>
+                  { i !== (identityDisplayOrder.length - 1) &&
+                    <Divider />
+                  }
+                </div>
+              }
+            </div>
+            )
+          })}
+          </List>
+        </Segment>
       </div>
     )
   }
