13c21c79b2edcd44fcaba357e59d3ac826507276
DanishAmjadAlvi <danishamjad@gmail.com>
2017-01-07 08:21:53 +0000

    Changed the format of both the Records and ApprovalQueue to optimise space-conservation

--- app/components/ApprovalQueue.jsx
+++ app/components/ApprovalQueue.jsx
@@ -23,38 +23,43 @@ export default class BK_ApprovalQueue extends React.Component {
             return (
               <div>
               <Grid celled container stackable reversed='mobile' columns={4}>
-                <Grid.Row>
                   <Grid.Column width={2}>
                     <Image src={'img/' + object.id + '.png'} shape='circular' size='small'/>
                   </Grid.Column>
                   <Grid.Column width={13}>
-                   <Header as='h1'>{object.company}</Header>
-                   <Header as='h2'><i>{object.role}</i></Header>
-                   <Header as='h3'><i>{object.location}</i></Header>
-                   <Grid columns={2}>
-                   <Grid.Column width={13}>
-                    </Grid.Column>
-                      <Grid.Column width={3}>
-                         <Container textAlign='left'>
-                           <Label as='a'>
-                                <Icon name='calendar' />Date
-                          </Label>
-                          {"     " + object.date}
-                        </Container>
-                        <br/>
-                        <Container textAlign='left'>
-                          <Label as='a'>
-                               <Icon name='clock' />Time
-                         </Label>
-                         {"     " + object.time}
-                        </Container>
-                        </Grid.Column>
-                    </Grid>
-                    <Container textAlign='justified'>
-                      <b>{object.recordType}</b>
-                    </Container>
+                      <Grid>
+                        <Grid.Row>
+                          <Grid.Column floated='left' width={10}>
+                            <Header as='h1'>{object.company}</Header>
+                            <Header as='h2'><i>{object.role}</i></Header>
+                            <Header as='h3'><i>{object.location}</i></Header>
+                          </Grid.Column>
+                          <Grid.Column floated='right' width={3}>
+                            <Container textAlign='left'>
+                              <Label as='a'>
+                                   <Icon name='calendar' />Date
+                             </Label>
+                             {"     " + object.date}
+                           </Container>
+                           <br/>
+                           <Container textAlign='left'>
+                             <Label as='a'>
+                                  <Icon name='clock' />Time
+                            </Label>
+                            {"     " + object.time}
+                           </Container>
+                          </Grid.Column>
+                        </Grid.Row>
+
+                        <Grid.Row>
+                          <Grid.Column floated='left' width={10}>
+                            <Container textAlign='justified'>
+                              <b>{object.recordType}</b>
+                            </Container>
+                          </Grid.Column>
+                        </Grid.Row>
+                      </Grid>
                   </Grid.Column>
-                </Grid.Row>
               </Grid>
             </div>
             );
--- app/components/records.jsx
+++ app/components/records.jsx
@@ -22,52 +22,57 @@ export default class BK_Records extends React.Component {
             return (
               <div>
               <Grid celled container stackable reversed='mobile' columns={4}>
-                <Grid.Row>
                   <Grid.Column width={2}>
                     <Image src={'img/' + record.id + '.png'} shape='circular' size='small'/>
                   </Grid.Column>
                   <Grid.Column width={13}>
-                   <Header as='h1'>{record.company}</Header>
-                   <Header as='h2'><i>{record.role}</i></Header>
-                   <Header as='h3'><i>{record.location}</i></Header>
-                   <Grid columns={2}>
-                   <Grid.Column width={13}>
-                    </Grid.Column>
-                      <Grid.Column width={3}>
-                         <Container textAlign='left'>
-                           <Label as='a'>
-                                <Icon name='calendar' />Date
-                          </Label>
-                          {"     " + record.date}
-                        </Container>
-                        <br/>
-                        <Container textAlign='left'>
-                          <Label as='a'>
-                               <Icon name='clock' />Time
-                         </Label>
-                         {"     " + record.time}
-                        </Container>
-                        </Grid.Column>
-                    </Grid>
-                    <Container textAlign='justified'>
-                      <b>{record.recordType}</b>
-                      <Divider />
-                      <Accordion>
-                        <Accordion.Title>
-                          <Icon name='dropdown' />
-                          See full {record.recordType}
-                        </Accordion.Title>
-                        <Accordion.Content>
-                          <p>
-                            {record.content}
-                          </p>
-                        </Accordion.Content>
-                        </Accordion>
-                    </Container>
+                      <Grid>
+                        <Grid.Row>
+                          <Grid.Column floated='left' width={10}>
+                            <Header as='h1'>{record.company}</Header>
+                            <Header as='h2'><i>{record.role}</i></Header>
+                            <Header as='h3'><i>{record.location}</i></Header>
+                          </Grid.Column>
+                          <Grid.Column floated='right' width={3}>
+                            <Container textAlign='left'>
+                              <Label as='a'>
+                                   <Icon name='calendar' />Date
+                             </Label>
+                             {"     " + record.date}
+                           </Container>
+                           <br/>
+                           <Container textAlign='left'>
+                             <Label as='a'>
+                                  <Icon name='clock' />Time
+                            </Label>
+                            {"     " + record.time}
+                           </Container>
+                          </Grid.Column>
+                        </Grid.Row>
+
+                        <Grid.Row>
+                          <Grid.Column floated='left' width={16}>
+                            <Container textAlign='justified'>
+                              <b>{record.recordType}</b>
+                              <Divider />
+                              <Accordion>
+                                <Accordion.Title>
+                                  <Icon name='dropdown' />
+                                  See full {record.recordType}
+                                </Accordion.Title>
+                                <Accordion.Content>
+                                  <p>
+                                    {record.content}
+                                  </p>
+                                </Accordion.Content>
+                                </Accordion>
+                            </Container>
+                          </Grid.Column>
+                        </Grid.Row>
+                      </Grid>
                   </Grid.Column>
-                </Grid.Row>
               </Grid>
-            </div>
+              </div>
             );
           })}
       </div>
