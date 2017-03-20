6a5181918472a8fb9d5cdfc52a2132f3e2aff1ef
George Pîrlea <george@dranov.ro>
2017-03-20 22:48:03 +0000

    Close record request modal on submit

--- app/components/request-button.jsx
+++ app/components/request-button.jsx
@@ -7,24 +7,31 @@ import BK_RequestForm from './request-form.jsx';
 export default class BK_RequestButton extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {isOpen: false};
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({isOpen: !this.state.isOpen});
   }
 
   render() {
     return (
-      <Modal trigger={
+      <Modal open={this.state.isOpen} trigger={
         <Button
           primary
           content='New request'
           icon='plus'
           labelPosition='left'
           id='bk-request-button'
+          onClick={this.toggleModal}
         />
       }>
         <Modal.Header>
           Create a new request
         </Modal.Header>
         <Modal.Content>
-          <BK_RequestForm />
+          <BK_RequestForm onChange={this.toggleModal}/>
         </Modal.Content>
       </Modal>
     )
