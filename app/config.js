a24134184a6b844c4f89f9e53c2303ed25cf939e
Alexis Enston <xn@outlook.com>
2017-03-21 16:17:00 +0000

    New testnet 4242

--- app/config.js
+++ app/config.js
@@ -2,4 +2,4 @@
 //var BkMainContractAddress = "0x3bad830ac5f1e37d8479533c6f5c5f51a2b29b0d";
 
 //private testnet
-var BkMainContractAddress = "0x6c1fa277ebe5a0685d0e52dded0c071f7b37e8d4";
+var BkMainContractAddress = "0x743fab9bc48d25641cf8be2e9c1afae589290acf";
