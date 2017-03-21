d809ae12e232d8f9409b43dfd4437b3ae9a0164a
Alexis Enston <xn@outlook.com>
2017-03-21 13:00:03 +0000

    private testnet bkMain

--- app/config.js
+++ app/config.js
@@ -1,2 +1,5 @@
 //BitKariero config
-var BkMainContractAddress = "0x3bad830ac5f1e37d8479533c6f5c5f51a2b29b0d";
+//var BkMainContractAddress = "0x3bad830ac5f1e37d8479533c6f5c5f51a2b29b0d";
+
+//private testnet
+var BkMainContractAddress = "0x6c1fa277ebe5a0685d0e52dded0c071f7b37e8d4";
