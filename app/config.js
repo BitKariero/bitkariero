ab6393f7aa69c3c166d93614e77091c319d6c0fb
Alexis Enston <xn@outlook.com>
2017-03-24 15:33:28 +0000

    Reset bkmain

--- app/config.js
+++ app/config.js
@@ -2,4 +2,4 @@
 //var BkMainContractAddress = "0x3bad830ac5f1e37d8479533c6f5c5f51a2b29b0d";
 
 //private testnet
-var BkMainContractAddress = "0x743fab9bc48d25641cf8be2e9c1afae589290acf";
+var BkMainContractAddress = "0x3be643d062f100233a52daac4c7294c7c1159971";
