b26a6688d74103ab0b42dfa65f4e0dd92921f6d7
Alexis Enston <xn@outlook.com>
2017-04-18 12:20:10 +0100

    fix up private network scripts

--- scripts/private-network-init.sh
+++ scripts/private-network-init.sh
@@ -1,4 +1,5 @@
 #private network 
 wget http://51.15.141.238/c3.tar
 tar -xvf c3.tar
-geth --datadir c3 --rpc --port "20202" --nodiscover  --rpcapi eth,web3,personal --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --ipcpath ~/.ethereum/geth.ipc
+geth --datadir c3 --networkid 4242 --rpc --port "20202" --nodiscover  --rpcapi eth,web3,personal --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --ipcpath ~/.ethereum/geth.ipc console
+
--- scripts/private-network-run.sh
+++ scripts/private-network-run.sh
@@ -1,2 +1,2 @@
 #private network 
-geth --datadir c3 --rpc --port "20202" --nodiscover  --rpcapi eth,web3,personal --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --ipcpath ~/.ethereum/geth.ipc --unlock 0
+geth --datadir c3 --networkid 4242 --rpc --port "20202" --nodiscover  --rpcapi eth,web3,personal --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --ipcpath ~/.ethereum/geth.ipc --unlock 0
