29449c92802d79607b3939ccf5eeebf542350d13
Alexis Enston <xn@outlook.com>
2017-04-18 12:07:01 +0100

    private net scripts

---
+++ scripts/private-network-init.sh
@@ -0,0 +1,4 @@
+#private network 
+wget http://51.15.141.238/c3.tar
+tar -xvf c3.tar
+geth --datadir c3 --rpc --port "20202" --nodiscover  --rpcapi eth,web3,personal --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --ipcpath ~/.ethereum/geth.ipc
